54d7f1022db04f3f17287356480509a38219026e
Balaji S <balaaagi@gmail.com>
2015-10-19 23:36:52 +0530

    Added StairCase  SOlution

---
+++ hackerrank/StairCase.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class StairCase {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in=new Scanner(System.in);
+        int N=in.nextInt();
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N-i;j++){
+                System.out.print(" ");
+
+            }
+            for(int k=1;k<=i;k++)
+                System.out.print("#");
+            System.out.println("");
+        }
+    }
+}
