4ab5c80ea8800bc4e3d85bc9a7fbc674fafbf828
Balaji Srinivasan <balaaagi@gmail.com>
2016-02-11 14:52:24 +0530

    Added Today's CC Attempts

---
+++ hackerrank/MinimumDraws.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/*
+Problem URL: https://www.hackerrank.com/challenges/minimum-draws
+*/
+public class MinimumDraws {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in =new Scanner(System.in);
+        int T=in.nextInt();
+        for(int i=0;i<T;i++){
+            int pairs=in.nextInt();
+            System.out.println(pairs+1);
+        }
+    }
+}
