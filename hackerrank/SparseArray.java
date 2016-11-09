c915c955e5c5856b8e77f8199c2824ef52d480fd
Balaji Srinivasan <balaaagi@gmail.com>
2016-11-09 19:01:11 +0530

    Implemented Sparse Arrays

--- hackerrank/SparseArray.java
+++ hackerrank/SparseArray.java
@@ -6,30 +6,57 @@ import java.util.regex.*;
 
 public class SparseArray {
 
+	public static String[] inputStrings;
+	public static int[] inputStringCount;
+	public static int N,start=0;
+
+	public static void add(String s){
+		boolean alreadyFound=false;
+		for(int i=0;i<N;i++){
+			if((inputStrings[i]!=null)&&(inputStrings[i].equals(s))){
+				inputStringCount[i]++;
+				alreadyFound=true;
+				break;
+			}
+		}
+		if(!alreadyFound){
+
+			inputStrings[start]=s;
+			inputStringCount[start]=1;
+			start++;	
+		}
+		
+
+	}
+
+
+	public static int query(String queryString){
+		int outputCount=0;
+		for(int i=0;i<N;i++){
+			if((inputStrings[i]!=null)&&(inputStrings[i].equals(queryString))){
+				outputCount=inputStringCount[i];
+				break;
+			}
+		}
+
+		return outputCount;
+
+	}
+/*Problem URL: https://www.hackerrank.com/challenges/sparse-arrays*/
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 		Scanner in=new Scanner(System.in);
-		int N=in.nextInt();
-		
-		HashMap<String,Integer> inputs=new HashMap<String,Integer>();
+		N=in.nextInt();
+		inputStrings=new String[N];
+		inputStringCount=new int[N];
 		for(int i=0;i<N;i++){
-			String inputString=in.next();
-			if(inputs.containsKey(inputString)){
-				Integer existingCount=inputs.get(inputString);
-				inputs.put(inputString,existingCount+1);
-			}				
-			else
-				inputs.put(inputString,1);
+			add(in.next());
 		}
 		int Q=in.nextInt();
-		int[] output=new int[Q];
 		for(int i=0;i<Q;i++){
-			String queryString=in.next();
-			if(inputs.containsKey(queryString))
-				System.out.println(inputs.get(queryString));
-			else
-				System.out.println(0);
+			System.out.println(query(in.next()));
 		}
+
 
     }
 }
